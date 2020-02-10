037092aca0fe4f4659e9e84ff9321123bf724e68
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-02-10 15:11:53 +0100

    Added support for storing dimensions of persistence diagrams

--- ephemeral/topology.py
+++ ephemeral/topology.py
@@ -237,6 +237,72 @@ def load_persistence_diagram_txt(filename, comment='#'):
     return pd
 
 
+def load_persistence_diagram_dipha(filename):
+    '''
+    Loads a persistence diagram from a file. The file is assumed to be
+    in DIPHA format.
+
+    TODO: extend this!
+    '''
+
+    def _read_int64(f):
+        return np.fromfile(f, dtype=np.int64, count=1)[0]
+
+    def _fromfile(f, dtype, count, skip):
+        data = np.zeros((count, ))
+
+        for c in range(count):
+            data[c] = np.fromfile(f, dtype=dtype, count=1)[0]
+            f.seek(f.tell() + skip)
+
+        return data
+
+    with open(filename, 'rb') as f:
+
+        magic_number = _read_int64(f)
+        file_id = _read_int64(f)
+
+        # Ensures that this is DIPHA file containing a persistence
+        # diagram, and nothing something else.
+        assert magic_number == 8067171840
+        assert file_id == 2
+
+        n_pairs = _read_int64(f)
+
+        # FIXME: this does *not* follow the original MATLAB script, but
+        # it produces the proper results.
+        dimensions = _fromfile(
+            f,
+            dtype=np.int64,
+            count=n_pairs,
+            skip=16
+        )
+
+        # Go back whence you came!
+        f.seek(0, 0)
+        f.seek(32)
+
+        creation_values = _fromfile(
+            f,
+            dtype=np.double,
+            count=n_pairs,
+            skip=16
+        )
+
+        # Go back whence you came!
+        f.seek(0, 0)
+        f.seek(40)
+
+        destruction_values = _fromfile(
+            f,
+            dtype=np.double,
+            count=n_pairs,
+            skip=16
+        )
+
+    return dimensions, creation_values, destruction_values
+
+
 def make_betti_curve(diagram, ignore_errors=False):
     '''
     Creates a Betti curve of a persistence diagram, i.e. a curve that

34cd50beba718cb8bd0f90a41be8bd7719f63a69
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-02-11 10:10:28 +0100

    Added utility function for filename parsing

---
+++ ephemeral/utilities.py
@@ -0,0 +1,37 @@
+'''
+Module containing utility functions.
+'''
+
+import re
+
+
+def parse_filename(filename):
+    '''
+    Parses a filename and composes it into different components.
+    Depending on the filename, the following information can be
+    extracted:
+
+    - Subject ID
+    - Task (abbreviation)
+    - Time step (optional)
+
+    These items will be returned as tuples. If time information
+    is not available, `None` will be returned.
+    '''
+
+    tokens = filename.split('_')
+
+    for token in tokens:
+        if 'sub' in token:
+            subject = re.match('\D*(\d+)\D*', token).group(1)
+        elif 'task' in token:
+            task = token.split('-')[1]
+
+    potential_time = tokens[-1]
+
+    if re.match('\d+', potential_time):
+        time = potential_time
+    else:
+        time = None
+
+    return subject, task, time

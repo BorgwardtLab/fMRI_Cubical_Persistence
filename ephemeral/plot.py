516048011c485cfbe3dfceeec194e8c1a97fcb3f
Christian Bock <chrs.bock@gmail.com>
2020-02-11 19:39:50 +0100

    Added utilities to plot persistence diagrams.

---
+++ ephemeral/plot.py
@@ -0,0 +1,82 @@
+from os.path import join
+from topology import load_persistence_diagram_dipha, load_persistence_diagram_json
+from utilities import parse_filename, get_patient_ids_and_times
+import numpy as np
+from glob import glob
+import seaborn as sns
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from collections import defaultdict
+
+import argparse
+
+def plot_pds(data_path: str, output_path: str='../figures/', dimensions: list=[0,1,2], task: str='pixar', masked: bool=True):
+    '''
+    Plots the persistence diagrams of all available patients
+    in one plot. 
+    '''
+
+    all_patients = get_patient_ids_and_times(data_path, task)
+    for dimension in dimensions:
+        fig = plt.figure(figsize=(40, 35))
+        min_c = float('inf')
+        min_d = float('inf')
+        max_c = -float('inf')
+        max_d = -float('inf')
+        for pat_idx, pat in tqdm(enumerate(list(all_patients.keys()))):
+            ax = fig.add_subplot(6, 5, pat_idx+1, projection='3d')
+            ax.set_title(f"Subject: {pat}")
+
+            ax.view_init(17, -60)
+            patient = {'c': [], 'd': [], 't': [], 'l': []} # Creation, Destruction, Time, Length
+
+            # Iterate in reverse order to have the first time point plotted on top
+            for time in all_patients[pat][::-1]:
+                f = join(data_path, f'sub-{task}{pat}_task-{task}_bold_space-MNI152NLin2009cAsym_preproc_{time}.json')
+
+                dim, creation, desctruction = load_persistence_diagram_json(f)
+
+                c = creation[np.argwhere(dim == dimension)].ravel()
+                d = desctruction[np.argwhere(dim == dimension)].ravel()
+
+                patient['c'] += c.tolist()
+                patient['d'] += d.tolist()
+                patient['t'] += [float(time)] * len(c)
+                patient['l'] += [len(c)]
+                
+                min_c = np.min([min_c, np.min(c)])
+                min_d = np.min([min_d, np.min(d)])
+                max_c = np.max([max_c, np.max(c)])
+                max_d = np.max([max_d, np.max(d)])
+        
+            # Generate color palette based on the number of time steps.
+            palette = sns.color_palette('Spectral_r', len(np.unique(patient['t'])))
+            colors = []
+
+            for i, l in enumerate(patient['l']):
+                colors += [palette[i]]*l
+
+            ax.scatter(patient['c'], patient['t'], patient['d'], c=colors, alpha=0.8, s=8)
+            ax.set_xlabel('Birth')
+            ax.set_ylabel('Time')
+            ax.set_zlabel('Death')
+            
+        for ax in fig.get_axes():
+            ax.set_xlim(min_c, max_c)
+            ax.set_zlim(min_d, max_d)
+        plt.tight_layout()
+        plt.savefig(join(output_path, f'{"masked" if masked else "raw"}_dimension_{dimension}.png'))
+    fig = plt.figure(figsize=(40, 35))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--raw', action='store_false', help="Whether to plot the raw data.")
+    parser.add_argument('-d', '--dimensions', nargs='+', default=[0,1,2], help="Which dimensions to plot.")
+    
+    args = parser.parse_args()
+    print(args.dimensions)
+    print(args.raw)
+    plot_pds(data_path="/Volumes/borgwardt/Projects/Ephemeral/Partly Cloudy/tda/raw/persistence_diagrams", dimensions=args.dimensions, masked=args.raw)
+
--- ephemeral/utilities.py
+++ ephemeral/utilities.py
@@ -4,6 +4,8 @@ Module containing utility functions.
 
 import os
 import re
+from collections import defaultdict
+from glob import glob
 
 
 def parse_filename(filename):
@@ -37,3 +39,22 @@ def parse_filename(filename):
         time = None
 
     return subject, task, time
+
+def get_patient_ids_and_times(path_to_data: str, task: str='pixar'):
+    '''
+    Returns a list of patient ids and available time steps
+    based on the data available.
+    '''
+    
+    all_patients = defaultdict(list)
+    for f in glob(os.path.join(path_to_data, f'sub-{task}*_task-{task}_bold_space-MNI152NLin2009cAsym_preproc_*.json')):
+        subject, task, time = parse_filename(f)
+        all_patients[subject].append(time)
+
+    all_patients = {k: sorted(v) for k, v in all_patients.items()}
+    sorted_patients = {}
+    for k in sorted(all_patients):
+        sorted_patients[k] = all_patients[k]
+
+    return sorted_patients
+

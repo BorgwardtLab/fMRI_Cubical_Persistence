5e968245d27d77abee72ddef34058865d5b71c2b
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-03-22 18:58:04 +0100

    Provided improved calculation of Betti curves
    This one does *not* get stuck when dealing with inconsistent creation
    times. It is capable of handling them quite well.

--- ephemeral/topology.py
+++ ephemeral/topology.py
@@ -491,21 +491,25 @@ def load_persistence_diagram_json(filename, return_raw=True):
                                             creation_values,
                                             destruction_values)
 
+def make_betti_curve(diagram):
+    """Create a Betti curve from a persistence diagram.
 
-def make_betti_curve(diagram, ignore_errors=False):
-    '''
     Creates a Betti curve of a persistence diagram, i.e. a curve that
     depicts the number of active intervals according to the threshold
     of the filtration.
 
-    :param diagram: Persistence diagram to convert
-    :param ignore_errors: If set, ignores errors instead of raising an
-    exception. This is only meant for debugging.
+    Parameters
+    ----------
+    diagram:
+        Persistence diagram to convert
 
-    :return: List of tuples of the form (x, y), where x refers to
-    a threshold, and y refers to a function value.
-    '''
-
+    Returns
+    -------
+    Betti curve of the input diagram, in the form of a `BettiCurve`
+    instance.
+    """
+    # Contains all potential event points, i.e. points at which the
+    # Betti curve might change.
     event_points = []
 
     for x, y in diagram:
@@ -519,8 +523,9 @@ def make_betti_curve(diagram, ignore_errors=False):
 
     # Create the 'raw' sequence of event points first. This blindly
     # assumes that all creation and destruction times are different
-    # from each other.
-
+    # from each other. If this is *not* the case, the same value is
+    # used with a different number of active intervals. This may be
+    # a problem for the consistency of indices later on.
     for p, is_generator in event_points:
         if is_generator:
             n_active += 1
@@ -529,29 +534,16 @@ def make_betti_curve(diagram, ignore_errors=False):
 
         output.append((p, n_active))
 
-    # Check some edge cases first: if both a generator and a destroyer
-    # share the same time, the barcode representation will not exhibit
-    # any gaps. This creates undesirable situations when creating each
-    # of the curves.
-
-    creation_times = set([t for t, g in event_points if g])
-    destruction_times = set([t for t, g in event_points if not g])
-
-    shared_times = creation_times.intersection(destruction_times)
-    if shared_times:
-        if ignore_errors:
-            pass
-        else:
-            raise RuntimeError('Inconsistent creation/destruction time')
-
     # If the diagram is empty, skip everything. In the following, I will
     # assume that at least a single point exists.
     if not event_points:
         return None
 
     prev_p = event_points[0][0]   # Previous time point
-    prev_v = event_points[0][1]   # Previous number of active intervals
+    prev_v = 0                    # Previous number of active intervals
 
+    # Will contain the tuples that give rise to the Betti curve in the
+    # end, i.e. the threshold and the number of active intervals.
     output_ = []
 
     # Functor that is called to simplify the loop processing, which
@@ -564,6 +556,8 @@ def make_betti_curve(diagram, ignore_errors=False):
         nonlocal prev_v
         nonlocal output_
 
+        # Update the number of active intervals for as long as the
+        # current threshold does *not* change.
         if prev_p == p:
             prev_v = n_active
 
@@ -571,7 +565,8 @@ def make_betti_curve(diagram, ignore_errors=False):
         # should now be stored.
         else:
 
-            # Create a transition point if this is not the first output
+            # Check whether this is *not* the first output and create
+            # a transition point in the data set.
             if output_:
 
                 # This makes the previous interval half-open by
@@ -607,53 +602,110 @@ def make_betti_curve(diagram, ignore_errors=False):
 
 
 class BettiCurve:
-    '''
-    Class representing a Betti curve in some dimension, i.e. a curve
+    """A Betti curve of a certain dimension.
+
+    This class is the main representation of a Betti curve, i.e. a curve
     that contains the number of active topological features at every
     point of a filtration process.
 
     This class provides some required wrapper functions to simplify,
     and improve, the usage of this concept.
-    '''
+    """
 
     def __init__(self, values):
-        '''
+        """Create a new Betti curve from a sequence of values.
+
         Creates a new Betti curve from a sequence of values. The values
         are supposed to be ordered according to their filtration value,
         such that the first dimension represents the filtration axis.
 
-        :param values: Input values
-        '''
-
+        Parameters
+        ----------
+        values:
+            Input values. This must be a sequence of tuples, with the
+            first dimension representing the threshold of a function,
+            and the second dimension representing the curve value. In
+            the function itself, `pd.DataFrame` will be used.
+        """
         if isinstance(values, pd.Series):
             self._data = values
 
             # It's brute force, but this ensures that the data frames
             # are compatible with each other.
-            assert self._data.index.name == 'x'
+            assert self._data.index.name == 'threshold'
 
         else:
             self._data = pd.DataFrame.from_records(
-                values, columns=['x', 'y'], index='x'
-            )['y']
+                values,
+                columns=['threshold', 'n_features'],
+                index='threshold'
+            )['n_features']
+
+    def __call__(self, threshold):
+        """Evaluate the Betti curve at a given threshold.
+
+        Parameters
+        ----------
+        threshold:
+            Threshold at which to evaluate the curve. All numbers are
+            valid here, but for some of them, the function may return
+            zero.
+
+        Returns
+        -------
+        Number of active features in the Betti curve under the given
+        threshold.
+        """
+        match = self._data[self._data.index == threshold]
+        if not match.empty:
+            return match.values[0]
+
+        # Interpolate between the nearest two indices. For most Betti
+        # curves, this should be the same value anyway, but if one is
+        # calculating averages, this might change.
+        else:
+            lower = self._data[self._data.index < threshold].index
+            upper = self._data[self._data.index > threshold].index
+
+            if not lower.empty and not upper.empty:
+                # Take the *last* index of the lower half of the data,
+                # and the *first* index of the upper half of the data,
+                # in order to find the proper neighbours.
+                lower = lower[-1]
+                upper = upper[0]
+
+                return 0.5 * (self._data[lower] + self._data[upper])
+            else:
+                # Either one of the indices is *outside* the halves of
+                # the data, so we return zero because the curve has to
+                # have compact support.
+                return 0.0
 
     def __repr__(self):
+        """Return a string-based representation of the curve."""
         return self._data.__repr__()
 
     def __add__(self, other):
-        '''
+        """Add a Betti curve to another Betti curve.
+
         Performs addition of two Betti curves. This necessitates
         re-indexing values accordingly in order to evaluate them
         properly.
 
         In case `other` is a number, does elementwise addition.
 
-        :param other: Betti curve to add to the current one
-        :return: Betti curve that results from the addition
-        '''
+        Parameters
+        ----------
+        other:
+            Betti curve to add to the current one, *or* a number, which
+            is added to *all* values of the current Betti curve.
 
+        Returns
+        -------
+        Betti curve that results from the addition.
+        """
         if isinstance(other, numbers.Number):
-            return BettiCurve(self._data + x)
+            return BettiCurve(self._data + other)
 
         # Not a number, so let's re-index the Betti curve and perform
         # addition for the new curves.
@@ -668,53 +720,69 @@ class BettiCurve:
         return BettiCurve(left + right)
 
     def __radd__(self, other):
+        """Arithmetic with Betti curves on the right-hand side."""
         if other == 0:
             return self
         else:
             return self.__add__(other)
 
     def __neg__(self):
-        '''
+        """Negate the current Betti curve.
+
         Negates the current values of the Betti curves, i.e. applies
         a unary minus operation to the curve.
 
-        :return: Negated Betti curve
-        '''
-
+        Returns
+        -------
+        Negated Betti curve
+        """
         return BettiCurve(-self._data)
 
     def __sub__(self, other):
+        """Subtract another Betti curve from the current one."""
         return self.__add__(-other)
 
     def __abs__(self):
-        '''
+        """Calculate absolute value of the Betti curve.
+
         Calculates the absolute value of the Betti curve. Does not
         modify the current Betti curve.
 
-        :return: Absolute value of the Betti curve
-        '''
-
+        Returns
+        -------
+        Absolute value of the Betti curve
+        """
         return BettiCurve(abs(self._data))
 
     def __truediv__(self, x):
-        '''
-        Elementwise division of a Betti curve by some number.
+        """Perform elementwise division of a Betti curve by some number.
 
-        :param x: Number to divide the Betti curve by
-        :return: Betti curve divided by x
-        '''
+        Parameters
+        ----------
+        x:
+            Number to divide the Betti curve by
 
+        Returns
+        -------
+        Betti curve divided by `x`
+        """
         return BettiCurve(self._data / x)
 
     def norm(self, p=1.0):
-        '''
+        """$L_p$ norm calculation for the Betti curve.
+
         Calculates an $L_p$ norm of the Betti curve and returns the
         result.
 
-        :param p: Exponent for the corresponding $L_p$ norm
-        :return: $L_p$ norm of the current Betti curve
-        '''
+        Parameters
+        ----------
+        p:
+            Exponent for the corresponding $L_p$ norm
 
+        Returns
+        -------
+        $L_p$ norm of the current Betti curve
+        """
         result = 0.0
         for (x1, y1), (x2, y2) in zip(
                 self._data.iteritems(),
@@ -735,14 +803,21 @@ class BettiCurve:
         return math.pow(result, 1.0 / p)
 
     def distance(self, other, p=1.0):
-        '''
+        """Calculate distance between two Betti curves.
+
         Calculates the distance between the current Betti curve and
         another one, subject to a certain $L_p$ norm.
 
-        :param other: Other Betti curve
-        :param p: Exponent for the corresponding $L_p$ norm
+        Parameters
+        ----------
+        other:
+            Other Betti curve
+        p:
+            Exponent for the corresponding $L_p$ norm
 
-        :return: Distance between the two curves
-        '''
-
+        Returns
+        -------
+        Distance between the two curves. This is based on the $L_p$ norm
+        of the difference curve.
+        """
         return abs(self - other).norm(p)

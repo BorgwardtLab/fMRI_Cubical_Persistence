477e1d6a8f1481a72390c1987b4a5eafb787b6ce
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-03-23 14:34:28 +0100

    Improved dendrogram plotting

--- ephemeral/plot_dendrogram.py
+++ ephemeral/plot_dendrogram.py
@@ -14,15 +14,16 @@ if __name__ == '__main__':
     parser.add_argument('FILE', type=str, help='Input file')
 
     args = parser.parse_args()
-
     data = np.loadtxt(args.FILE)
-    dendrogram(data, truncate_mode=None)
 
     # Ensures that we do not overwrite anything for the subsequent
     # output.
     basename = os.path.basename(args.FILE)
     basename = os.path.splitext(basename)[0]
     basename = basename.replace('Linkage_matrix', 'Dendrogram')
+
+    fig, ax = plt.subplots(figsize=(20, 10))
+    dendrogram(data, truncate_mode=None, ax=ax)
 
     plt.title(basename)
     plt.savefig(basename + '.png')

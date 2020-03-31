5b21e908f24e2ea93dd17eea56c5cf645e0c12eb
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-03-31 16:07:48 +0200

    Skeleton for embedding calculation script

---
+++ ephemeral/calculate_embeddings.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+#
+# Calculates embeddings of sets of persistence diagrams, based on
+# a pre-defined feature creation strategy.
+
+import argparse
+import collections
+import glob
+import json
+import os
+
+from topology import load_persistence_diagram_dipha
+from topology import load_persistence_diagram_json
+
+from utilities import parse_filename
+
+from tqdm import tqdm
+
+
+def create_feature_vectors(diagrams):
+    """Create feature vectors of sequence of diagrams."""
+    pass
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-i',
+        '--input',
+        required=True,
+        type=str,
+        help='Input directory'
+    )
+
+    parser.add_argument(
+        '-d', '--dimension',
+        type=int,
+        default=2,
+        help='Dimension to select from the persistence diagrams. Only tuples '
+             'of this dimension will be considered.'
+    )
+
+    parser.add_argument(
+        '-o', '--output',
+        type=str,
+        help='Output file for storing embedded points',
+        required=True,
+    )
+
+    args = parser.parse_args()
+
+    # Will store all persistence diagrams, ordered by subject. The keys
+    # are the subject identifiers, extracted from the filename, whereas
+    # the values are the persistence diagrams stored for them. Ordering
+    # of the diagrams follows the time step information.
+    diagrams_per_subject = collections.defaultdict(list)
+
+    filenames = sorted(glob.glob(os.path.join(args.input, '*.json')))
+    for filename in tqdm(filenames[:10], desc='File'):
+
+        subject, _, time = parse_filename(filename)
+        extension = os.path.splitext(filename)[1]
+
+        if extension == '.bin':
+            load_persistence_diagram_fn = load_persistence_diagram_dipha
+        elif extension == '.json':
+            load_persistence_diagram_fn = load_persistence_diagram_json
+
+        persistence_diagrams = load_persistence_diagram_fn(
+            filename,
+            return_raw=False
+        )
+
+        for diagram in persistence_diagrams:
+            if diagram.dimension == args.dimension:
+                diagrams_per_subject[subject].append(diagram)
+
+    data = create_feature_vectors(diagrams_per_subject)
+
+    with open(args.output, 'w') as f:
+        json.dump(data, f, indent=4)

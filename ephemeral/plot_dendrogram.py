fca6c747d385884aeb8aec56cb6a7d8c34ce2ee0
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-04-17 17:10:48 +0200

    Improved dendrogram output

--- ephemeral/plot_dendrogram.py
+++ ephemeral/plot_dendrogram.py
@@ -3,27 +3,44 @@
 import argparse
 import os
 
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
+from scipy.cluster.hierarchy import set_link_color_palette
 from scipy.cluster.hierarchy import dendrogram
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('FILE', type=str, help='Input file')
+    parser.add_argument('FILE', nargs='+', type=str, help='Input file')
 
     args = parser.parse_args()
-    data = np.loadtxt(args.FILE)
 
-    # Ensures that we do not overwrite anything for the subsequent
-    # output.
-    basename = os.path.basename(args.FILE)
-    basename = os.path.splitext(basename)[0]
-    basename = basename.replace('Linkage_matrix', 'Dendrogram')
+    for filename in args.FILE:
+        data = np.loadtxt(filename)
 
-    fig, ax = plt.subplots(figsize=(20, 10))
-    dendrogram(data, truncate_mode=None, ax=ax)
+        # Ensures that we do not overwrite anything for the subsequent
+        # output.
+        basename = os.path.basename(filename)
+        basename = os.path.splitext(basename)[0]
+        basename = basename.replace('Linkage_matrix', 'Dendrogram')
 
-    plt.title(basename)
-    plt.savefig(basename + '.png')
+        fig, ax = plt.subplots(figsize=(20, 10))
+        dendrogram(
+            data,
+            truncate_mode=None,
+            ax=ax,
+            show_leaf_counts=True,
+        )
+
+        ax.spines['top'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+
+        ax.get_yaxis().set_ticks([])
+
+        plt.title(basename)
+        plt.tight_layout()
+        plt.savefig(basename + '.svg', bbox_inches='tight')

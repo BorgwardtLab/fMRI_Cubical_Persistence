18a7e5ad88239d398f7e317f7f714ceeeab6a20f
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-05-04 15:40:46 +0200

    No normalisation for now (for embeddings)

--- ephemeral/scripts/calculate_embeddings.sh
+++ ephemeral/scripts/calculate_embeddings.sh
@@ -4,7 +4,7 @@
 for ENCODER in 'pca' 'mds' 'phate' 'm-phate'; do
   for DIMENSION in 2 3; do
     for GLOBAL in "" "-g"; do
-      for FILE in "../../results/persistence_images/brainmask_normalised_sigma1.0_r20.json" "../../results/persistence_images/occipitalmask_normalised_sigma1.0_r20.json"; do
+      for FILE in "../../results/persistence_images/brainmask_sigma1.0_r20.json" "../../results/persistence_images/occipitalmask_sigma1.0_r20.json"; do
         echo "encoder = $ENCODER, dimension = $dimension, file = $FILE"
         poetry run python ../visualise_embeddings.py -d $DIMENSION -e $ENCODER $GLOBAL $FILE 
       done

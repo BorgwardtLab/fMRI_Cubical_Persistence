84460908d3d7211436529a8af330ebe5725d34e5
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-05-04 15:29:34 +0200

    Added better output for script

--- ephemeral/scripts/calculate_embeddings.sh
+++ ephemeral/scripts/calculate_embeddings.sh
@@ -5,6 +5,7 @@ for ENCODER in 'pca' 'mds' 'phate' 'm-phate'; do
   for DIMENSION in 2 3; do
     for GLOBAL in "" "-g"; do
       for FILE in "../../results/persistence_images/brainmask_normalised_sigma1.0_r20.json" "../../results/persistence_images/occipitalmask_normalised_sigma1.0_r20.json"; do
+        echo "encoder = $ENCODER, dimension = $dimension, file = $FILE"
         poetry run python ../visualise_embeddings.py -d $DIMENSION -e $ENCODER $GLOBAL $FILE 
       done
     done

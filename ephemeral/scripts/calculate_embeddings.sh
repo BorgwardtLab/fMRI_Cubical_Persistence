96babfc9068b76133a1cad279a2f71b809d244c0
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-05-04 15:28:36 +0200

    Added script for embedding calculation

---
+++ ephemeral/scripts/calculate_embeddings.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+
+for ENCODER in 'pca' 'mds' 'phate' 'm-phate'; do
+  for DIMENSION in 2 3; do
+    for GLOBAL in "" "-g"; do
+      for FILE in "../../results/persistence_images/brainmask_normalised_sigma1.0_r20.json" "../../results/persistence_images/occipitalmask_normalised_sigma1.0_r20.json"; do
+        poetry run python ../visualise_embeddings.py -d $DIMENSION -e $ENCODER $GLOBAL $FILE 
+      done
+    done
+  done
+done

6968771f26b4ee378c3426ac1b50599c2b3cfeac
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-05-14 11:29:24 +0200

    Extended output for cluster matrix embedding scheme

--- ephemeral/embed_cluster_distance_matrices.py
+++ ephemeral/embed_cluster_distance_matrices.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
 
 import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
 
 from sklearn.manifold import MDS
 
@@ -14,8 +18,26 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     D = np.loadtxt(args.INPUT)
+    groups = pd.read_csv('../data/participant_groups.csv')['cluster']
 
-    mds = MDS(random_state=42, dissimilarity='precomputed')
-    X = mds.fit_transform(D)
+    # TODO: make embedding configurable?
+    clf = MDS(random_state=5, dissimilarity='precomputed', metric=True)
+    X = clf.fit_transform(D)
 
-    print(X)
+    X = np.append(X, groups.to_numpy().reshape(-1, 1), axis=1)
+
+    df = pd.DataFrame(X, columns=['x', 'y', 'group'])
+    df['group'] = df['group'].astype('int')
+
+    basename = os.path.basename(args.INPUT)
+    basename = os.path.splitext(basename)[0]
+
+    df.to_csv(
+        f'Embedding_{basename}.csv',
+        index=False,
+        float_format='%.04f',
+    )
+
+    plt.scatter(X[:, 0], X[:, 1], c=groups, cmap='Set1')
+    plt.colorbar()
+    plt.show()

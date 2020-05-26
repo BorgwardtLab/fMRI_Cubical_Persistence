ca102251b7c5d0e75c54d02e1bf86615b2d72409
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-05-26 18:54:11 +0200

    Storing file in compressed format to obtain autocorrelation baseline

--- ephemeral/baseline_autocorrelation.py
+++ ephemeral/baseline_autocorrelation.py
@@ -8,18 +8,14 @@
 # can subsequently be used in additional processing and analysis tasks.
 
 import argparse
-import math
-import numbers
 import os
 import warnings
 
 import nilearn as nl
 import numpy as np
 
-from tqdm import tqdm
 
-
-def mask_image(image_filename, mask_filename, mask_value=np.nan):
+def mask_image(image_filename, mask_filename):
     """Mask image and return it.
 
     Given an image filename and a mask filename, performs the masking
@@ -76,32 +72,6 @@ def basename(filename):
     return filename
 
 
-def format_time(time, n_time_steps):
-    """Format time index based on data availability.
-
-    Formats a time index according to a maximum number of time steps in
-    in the data. This is useful in order to ensure consistent filenames
-    of the *same* length.
-
-    Parameters
-    ----------
-    time:
-        Current time step that should be formatted
-
-    n_time_steps:
-        Maximum number of time steps
-
-    Returns
-    -------
-    Formatted string with added zeroes for padding. For example, if
-    there are 23 time steps in total, time step 5 will be formatted
-    as  '05', thereby ensuring that a lexicographical ordering will
-    be sufficient to sort the resulting files.
-    """
-    n_digits = int(math.log10(n_time_steps) + 1)
-    return f'{time:0{n_digits}d}'
-
-
 if __name__ == '__main__':
 
     # `nilearn` and `nibabel` have a compatibility issue concerning the
@@ -121,7 +91,8 @@ if __name__ == '__main__':
     parser.add_argument(
         '-m', '--mask',
         type=str,
-        help='Optional image mask to load'
+        help='Image mask to load',
+        required=True
     )
 
     parser.add_argument(
@@ -134,24 +105,12 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    if args.mask:
-        image = mask_image(args.image, args.mask, mask_value=args.mask_value)
-    else:
-        image = nl.image.load_img(args.image)
+    image = mask_image(args.image, args.mask)
+    X = np.corrcoef(image)
 
-    # The last dimension is always used as the time series dimension.
-    # Technically, one could do additional checks here.
-    n_time_steps = image.shape[-1]
+    # Build a nice filename such that all output files are stored
+    # correctly.
+    filename = f'{basename(args.image)}.npz'
+    filename = os.path.join(args.output, filename)
 
-    os.makedirs(args.output, exist_ok=True)
-
-    for index, i in enumerate(tqdm(nl.image.iter_img(image),
-                                   desc='Converting', total=n_time_steps)):
-
-        # Build a nice filename such that all output files are stored
-        # correctly.
-        time = format_time(index, n_time_steps)
-        filename = f'{basename(args.image)}_{time}.bin'
-        filename = os.path.join(args.output, filename)
-
-        to_dipha_format(i, f'{filename}', args.superlevel, args.normalise)
+    np.savez(filename, X=X)

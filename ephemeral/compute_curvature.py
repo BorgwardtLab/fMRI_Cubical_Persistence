4508d0190808eb6d7f5450190002a698542d2997
Christian Bock <christian.bock@bsse.ethz.ch>
2020-05-26 13:56:52 +0200

    Added plotting of debug visualization for curvature computation

--- ephemeral/compute_curvature.py
+++ ephemeral/compute_curvature.py
@@ -8,6 +8,7 @@ from os.path import basename
 from os.path import splitext
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from curvature import  arc_chord_ratio
 
@@ -20,6 +21,11 @@ if __name__ == '__main__':
     trajs = pd.read_csv(args.FILE)
 
     results = []
+    single_results = []
+
+    # Debug visualization
+    #fig, axes = plt.subplots(2, 3, figsize=(7, 5), sharey=True)
+    #axes = axes.flatten()
     for cohort in trajs.cohort.unique():
         vals_all = trajs.query('cohort == @cohort')
         vals_active = trajs.query('cohort == @cohort and time >= 7')
@@ -29,12 +35,19 @@ if __name__ == '__main__':
         vals_active = vals_active.sort_values(by='time')[['x', 'y']].values
 
         # Compute curvature
-        curv_all = arc_chord_ratio(vals_all)
-        curv_active = arc_chord_ratio(vals_active)
+        curv_all, _, _ = arc_chord_ratio(vals_all, True)
+        curv_active, l_active, last = arc_chord_ratio(vals_active, True)
         results.append({'cohort': cohort, 'curv_all': curv_all,
                         'curv_active': curv_active})
+
+        #axes[cohort].plot(l_active)
+        #axes[cohort].axhline(last)
+        #axes[cohort].set_title(f'Indiv. distances Cohort {cohort}')
 
     filename = splitext(basename(args.FILE))[0]
     results = pd.DataFrame(results)
     results.to_csv(f'../results/cohort_trajectories/{filename}_curv.csv',
                    index=False)
+
+    #plt.tight_layout()
+    #plt.savefig(f'../figures/{filename}_curv.pdf')

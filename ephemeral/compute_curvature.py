91cef4e83b40f0af2c364ad8de647562644d64ff
Christian Bock <christian.bock@bsse.ethz.ch>
2020-05-26 14:37:11 +0200

    Allow for window when computing curvature

--- ephemeral/compute_curvature.py
+++ ephemeral/compute_curvature.py
@@ -9,6 +9,7 @@ from os.path import splitext
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from IPython import embed
 
 from curvature import  arc_chord_ratio
 
@@ -16,6 +17,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('FILE', type=str, help="File with X,Y coordinates and"
                         "group assignment.")
+    parser.add_argument('-w', '--window', type=int, help="Time window length"
+                       " over which the curvatue is computed. If 0, curvature"
+                        " is computed over the whole TS.")
 
     args = parser.parse_args()
     trajs = pd.read_csv(args.FILE)
@@ -35,16 +39,35 @@ if __name__ == '__main__':
         vals_active = vals_active.sort_values(by='time')[['x', 'y']].values
 
         # Compute curvature
-        curv_all, _, _ = arc_chord_ratio(vals_all, True)
-        curv_active, l_active, last = arc_chord_ratio(vals_active, True)
-        results.append({'cohort': cohort, 'curv_all': curv_all,
-                        'curv_active': curv_active})
+        if args.window:
+            # First for all values
+            end = vals_all.shape[0] - args.window
+            for idx, start in enumerate(range(end + 1)):
+                v_all = vals_all[start:start + args.window]
+                curv_all = arc_chord_ratio(v_all)
+                results.append({'cohort': cohort, 'idx': idx,
+                                  'curv_all': curv_all})
+
+            # Then for active values
+            end = vals_active.shape[0] - args.window
+            for idx, start in enumerate(range(end + 1)):
+                v_active = vals_active[start:start + args.window]
+                curv_active = arc_chord_ratio(v_active)
+                results.append({'cohort': cohort, 'idx': idx,
+                                  'curv_active': curv_active})
+
+        else:
+            curv_all, _, _ = arc_chord_ratio(vals_all, True)
+            curv_active, l_active, last = arc_chord_ratio(vals_active, True)
+            results.append({'cohort': cohort, 'curv_all': curv_all,
+                            'curv_active': curv_active})
 
         #axes[cohort].plot(l_active)
         #axes[cohort].axhline(last)
         #axes[cohort].set_title(f'Indiv. distances Cohort {cohort}')
 
     filename = splitext(basename(args.FILE))[0]
+    filename += f'_{args.window}' if args.window else ''
     results = pd.DataFrame(results)
     results.to_csv(f'../results/cohort_trajectories/{filename}_curv.csv',
                    index=False)

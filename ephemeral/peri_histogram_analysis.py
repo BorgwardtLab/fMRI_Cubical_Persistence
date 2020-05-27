6869bbac574213baedbaa01df45b9745bda3db77
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-05-27 10:31:36 +0200

    Converted peri-event analysis to work with curves

--- ephemeral/peri_histogram_analysis.py
+++ ephemeral/peri_histogram_analysis.py
@@ -3,14 +3,32 @@
 # Performs peri-event histogram analysis for a variability curve. This
 # code was kindly contributed by Tristan Yates.
 
+import argparse
+import os
+
 import numpy as np
 import pandas as pd
 
+import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 
+from scipy.stats import sem
 
-def get_salience_indices():
-    """Return indices with salient events."""
+
+def get_salience_indices(threshold=7):
+    """Return indices with salient events (using an optional threshold).
+
+    Parameters
+    ----------
+    threshold : int
+        Threshold for salience calculation. If the number of votes is
+        greater than or equal to the threshold, the event is reported
+        in the result.
+
+    Returns
+    -------
+    Indices at which salient events happen.
+    """
     df_annot = pd.read_excel('../data/annotations.xlsx')
 
     # Get the salience values; it is perfectly justified to replace NaN
@@ -21,7 +39,7 @@ def get_salience_indices():
     # These are the detected event boundaries, according to Tristan's
     # analysis. Note that since the index has been shifted above, the
     # dropping operation does *not* have to be considered here!
-    salience_indices, = np.nonzero(salience >= 7)
+    salience_indices, = np.nonzero(salience >= threshold)
 
     # Again, no shift here!
     return salience_indices
@@ -29,55 +47,40 @@ def get_salience_indices():
 
 if __name__ == '__main__':
 
-    # Get the variability data
-    which_type='individual'
-    smooth='yes'
-    high_sal='no'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('INPUT', type=str, help='Input file')
 
-    if which_type=='individual':
-        # All subs
-        mask='occipitalmask' # occipitalmask or brainmask
-        measure='summary_statistics' # persistence_images or summary_statistics
-       
-        if smooth=='yes':
-            variability_time=pd.read_csv('variability/Variability_%s_%s_smooth3.csv' % (mask,measure))
-        else:
-            variability_time=pd.read_csv('variability/Variability_%s_%s.csv' % (mask,measure))
-    elif which_type=='cohort':
-        mask='brainmask'
-        measure='cohort_r3' # persistence_images or summary_statistics
+    args = parser.parse_args()
 
-        variability_time=pd.read_csv('variability/%s_%s.csv' % (mask,measure))
+    variability_curve = pd.read_csv(args.INPUT)
+    event_boundaries = get_salience_indices()
 
-    variability_time.head()
+    # This is the maximum time stored in the data set; it does not
+    # necessarily correspond to the length of the curve because it
+    # is possible that indices have been dropped.
+    T = variability_curve['time'].max()
 
-    # Get the boundaries 
-    event_boundaries=np.load('event_boundaries_not_shifted.npy') #starts at first movie time point, not at blank period
-    print('Event boundaries:',event_boundaries)
-
-    highsal_event_boundaries=np.load('highsal_event_boundaries_not_shifted.npy')
-    print('Most salient event boundaries:',highsal_event_boundaries)
-
-    # Do you want the highly salient events or all event boundaries?
-    if high_sal=='yes':
-        test_boundaries=highsal_event_boundaries
-    else:
-        test_boundaries=event_boundaries
+    # Makes it easier to access a given time step; note that we are
+    # using the indices from the event boundaries here.
+    variability_curve.set_index('time', inplace=True)
 
     # Make the peri-event curve
-    peri_event=np.zeros((w*2+1,len(test_boundaries)))
+    w = 3
+    peri_event = np.zeros((w*2 + 1, len(event_boundaries)))
 
-    for idx,t in enumerate(range(-w,w+1)):
-       
-        for eb, bound in enumerate(test_boundaries):
-           
-            if bound+t<0 or bound+t > len(variability_shifted)-1:
-                peri_event[idx,eb]=np.nan
+    for idx, t in enumerate(range(-w, w+1)):
+        for eb, bound in enumerate(event_boundaries):
+            if bound + t < 0 or bound + t > T:
+                peri_event[idx, eb] = np.nan
             else:
-                peri_event[idx,eb]=variability_shifted[bound+t]
+                peri_event[idx, eb] = variability_curve.loc[bound + t]
 
-    plt.title('%s %s' %(mask,measure))
+    name = os.path.splitext(os.path.basename(args.INPUT))[0]
+    plt.title(name)
+
     colors = cm.coolwarm(np.linspace(0, 1, peri_event.shape[0]))
     plt.vlines(0,np.nanmin(peri_event),np.nanmax(peri_event),linestyle='dashed')
-    plt.bar(np.arange(-w,w+1),np.nanmean(peri_event,axis=1),yerr=stats.sem(peri_event,axis=1,nan_policy='omit'),color=colors)
+    plt.bar(np.arange(-w,w+1),np.nanmean(peri_event,axis=1),yerr=sem(peri_event, axis=1, nan_policy='omit'),color=colors)
     plt.ylim(np.nanmin(peri_event),np.nanmax(peri_event))
+
+    plt.show()

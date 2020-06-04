ea7ef4404bd17ee4f4c5aae66af693a1a8d2f052
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-06-04 13:24:59 +0200

    Reporting mean number of points in dimension 2

--- ephemeral/summary_persistence_diagrams.py
+++ ephemeral/summary_persistence_diagrams.py
@@ -1,5 +1,7 @@
 import argparse
 
+import numpy as np
+
 from topology import load_persistence_diagram_json
 
 
@@ -13,9 +15,15 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    counts = []
+
     for filename in args.FILES:
         dimensions, _, _ = load_persistence_diagram_json(filename)
         unique_dimensions = set(dimensions)
 
+        counts.append(len(dimensions == 2))
+
         for dimension in unique_dimensions:
             print(f'{dimension}: {len(dimensions == dimension)}')
+
+    print(np.mean(counts))

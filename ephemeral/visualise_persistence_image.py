0aba5da75d94ad1300564ee3cbcb00ace04c1b75
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-06-10 18:47:00 +0200

    Adding proper persistence image output
    This can be directly used for TikZ and pgfplots, making it easy to
    create additional supplemental figures.

--- ephemeral/visualise_persistence_image.py
+++ ephemeral/visualise_persistence_image.py
@@ -30,45 +30,7 @@ if __name__ == '__main__':
     X = clf.transform([pd])[0]
 
     with open(f'PI_{args.resolution}.txt', 'w') as f:
-        for j in range(n):
-            for i in range(n):
-                print(f'{i} {j} {mean[j, i]}', file=f)
+        for j in range(args.resolution):
+            for i in range(args.resolution):
+                print(f'{i} {j} {X[j, i]}', file=f)
             print('', file=f)
-
-
-    raise 'heck'
-
-    for subject in tqdm(sorted(data.keys()), desc='Subject'):
-
-        # Check whether we are dealing with a proper subject, or an
-        # information key in the data.
-        try:
-            _ = int(subject)
-        except ValueError:
-            continue
-
-        images = [np.array(X) for X in data[subject]]
-        X = sum(images) / len(images)
-        r = int(np.sqrt(X.shape[0]))
-        X = X.reshape(r, r)
-
-        output = f'../results/average_persistence_images/{basename}'
-        os.makedirs(output, exist_ok=True)
-
-        #for index, image in enumerate(images):
-        #    plt.imshow(image.reshape(r, r), cmap='Spectral')
-        #    plt.axis('off')
-        #    plt.tight_layout()
-        #    plt.savefig(
-        #        f'{output}/{subject}_{index:03d}.png',
-        #        bbox_inches=0,
-        #    )
-
-        plt.imshow(X, cmap='Spectral')
-
-        if show_title:
-            plt.title(subject)
-
-        plt.axis('off')
-        plt.tight_layout()
-        plt.savefig(f'{output}/{subject}.png', bbox_inches='tight')
